16f9a61905fcaacd80cdc1300a079e868b49b1f7
Kazi Farhan Hoque <107494197+KaziFarhanHo@users.noreply.github.com>
2024-05-14 20:20:34 -0400

    Create HW1.sql

---
+++ Scripts/HW1.sql
@@ -0,0 +1,33 @@
+-- SQL Script to create a Data Warehouse in Google BigQuery within the 'cis4400' dataset
+
+-- Create a Date Dimension Table in the 'cis4400' dataset
+CREATE TABLE cis4400.DateDimension (
+    DateKey STRING NOT NULL,
+    Date DATE,
+    Year INT64,
+    Quarter INT64,
+    Month INT64,
+    Day INT64,
+    WeekdayName STRING
+) OPTIONS(
+    description="A dimension table for dates"
+);
+
+-- Create an Incidents Fact Table in the 'cis4400' dataset
+CREATE TABLE cis4400.IncidentsFact (
+    IncidentKey STRING NOT NULL,
+    DateKey STRING,
+    SomeMeasure INT64,
+    OtherMeasure INT64
+) OPTIONS(
+    description="A fact table for incidents"
+);
+
+
+-- Insert a sample date into DateDimension
+INSERT INTO cis4400.DateDimension (DateKey, Date, Year, Quarter, Month, Day, WeekdayName)
+VALUES (GENERATE_UUID(), '2023-10-01', 2023, 4, 10, 1, 'Sunday');
+
+-- Insert a sample incident into IncidentsFact linking to the DateDimension
+INSERT INTO cis4400.IncidentsFact (IncidentKey, DateKey, SomeMeasure, OtherMeasure)
+VALUES (GENERATE_UUID(), (SELECT DateKey FROM cis4400.DateDimension WHERE Date = '2023-10-01'), 100, 200);
